de530593fb8e516929bbc0e3340372c8f024e788
dv-jamie <sjm2506@gmail.com>
2024-07-12 16:37:34 +0900

    배경 작업

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Detail from './components/Detail';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 
+import './common.css';
+import styles from './App.module.css';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Cover />
-      <Detail />
-      <Gallery />
-      <Contact />
+    <div className={styles.background}>
+      <div className={styles.content_wrap}>
+        <Header />
+        <Cover />
+        <Detail />
+        <Gallery />
+        <Contact />
+      </div>
     </div>
   );
 }
