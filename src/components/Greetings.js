edfe8982597767f6bbc4a90771c49caf0d775e7a
dv-jamie <sjm2506@gmail.com>
2024-07-31 14:06:09 +0900

    안내사항 및 식순 추가

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import camera from '../assets/icons/camera.svg';
+import Order from './Order';
+
 import styles from './Greetings.module.css';
 
 function Greetings() {
@@ -11,6 +14,17 @@ function Greetings() {
             귀한 시간 내어주시는 만큼<br />
             감사한 마음 간직하며 살겠습니다.
         </p>
+
+        <Order />
+
+        <div className={styles.information}>
+            <img src={camera} alt="카메라 아이콘" />
+            <p>
+                결혼식 당일 포토부스가 있습니다.<br />
+                사진과 함께 따뜻한 말씀 남겨주시면<br />
+                소중히 간직하겠습니다.
+            </p>
+        </div>
     </div>
   );
 }
