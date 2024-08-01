588cd10e6ffe9bc40211d5b3390ed6da858e3470
dv-jamie <sjm2506@gmail.com>
2024-08-01 16:43:16 +0900

    갤러리 기본템플릿 추가

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import img1 from '../assets/images/self3.jpg';
+import img2 from '../assets/images/self1.jpg';
+import img3 from '../assets/images/self2.jpg';
+import img4 from '../assets/images/self4.jpg';
+
 import styles from './Gallery.module.css';
 
 function Gallery() {
@@ -7,9 +12,32 @@ function Gallery() {
     <section className={styles.gallery}>
       <h3 className={styles.caption}><span>GALLERY</span></h3>
       
-      <div className={styles.image_wrap}>
+      <ul className={styles.image_wrap}>
+        <li>
+          <img src={img1} alt="이미지1" />
+          <span>2020</span>
+        </li>
 
-      </div>
+        <li>
+          <img src={img2} alt="이미지2" />
+          <span>2021</span>
+        </li>
+
+        <li>
+          <img src={img3} alt="이미지3" />
+          <span>2022</span>
+        </li>
+
+        <li>
+          <img src={img4} alt="이미지4" />
+          <span>2023</span>
+        </li>
+
+        <li>
+          <img src={img1} alt="이미지1" />
+          <span>2024</span>
+        </li>
+      </ul>
     </section>
   );
 }
