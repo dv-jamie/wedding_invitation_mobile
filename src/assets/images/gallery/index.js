a9f449e099f66fb91d69c79b7ba90d5211afca3c
dv-jamie <sjm2506@gmail.com>
2024-08-28 04:52:36 +0900

    갤러리 영역 수정 및 사진 일부 추가

---
+++ src/assets/images/gallery/index.js
@@ -0,0 +1,38 @@
+import paris1 from './paris_1.jpg';
+import paris2 from './paris_2.jpg';
+import paris3 from './paris_3.jpg';
+import paris4 from './paris_4.jpg';
+import paris5 from './paris_5.jpg';
+import paris6 from './paris_6.jpg';
+import paris7 from './paris_7.jpg';
+import paris8 from './paris_8.jpg';
+import paris9 from './paris_9.jpg';
+import paris10 from './paris_10.jpg';
+import rome1 from './rome_1.jpg';
+import rome2 from './rome_2.jpg';
+import rome3 from './rome_3.jpg';
+import rome4 from './rome_4.jpg';
+import rome5 from './rome_5.jpg';
+import rome6 from './rome_6.jpg';
+import rome7 from './rome_7.jpg';
+import rome8 from './rome_8.jpg';
+import rome9 from './rome_9.jpg';
+import rome10 from './rome_10.jpg';
+import rome11 from './rome_11.jpg';
+import rome12 from './rome_12.jpg';
+import rome13 from './rome_13.jpg';
+import rome14 from './rome_14.jpg';
+import rome15 from './rome_15.jpg';
+import rome16 from './rome_16.jpg';
+import rome17 from './rome_17.jpg';
+import rome18 from './rome_18.jpg';
+import rome19 from './rome_19.jpg';
+import rome20 from './rome_20.jpg';
+import rome21 from './rome_21.jpg';
+import rome22 from './rome_22.jpg';
+import rome23 from './rome_23.jpg';
+
+export {
+  paris1, paris2, paris3, paris4, paris5, paris6, paris7, paris8, paris9, paris10,
+  rome1, rome2, rome3, rome4, rome5, rome6, rome7, rome8, rome9, rome10, rome11, rome12, rome13, rome14, rome15, rome16, rome17, rome18, rome19, rome20, rome21, rome22, rome23,
+};
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,10 @@
 import React, { useRef, useState } from 'react';
 
+import {
+  paris1, paris2, paris3, paris4, paris5, paris6, paris7, paris8, paris9, paris10,
+  rome1, rome2, rome3, rome4, rome5, rome6, rome7, rome8, rome9, rome10, rome11, rome12, rome13, rome14, rome15, rome16, rome17, rome18, rome19, rome20, rome21, rome22, rome23,
+} from '../assets/images/gallery';
+
 import prevArrow from '../assets/icons/prev_arrow.svg';
 import nextArrow from '../assets/icons/next_arrow.svg';
 import img1 from '../assets/images/self3.jpg';
@@ -10,63 +15,6 @@ import img4 from '../assets/images/self4.jpg';
 import styles from './Gallery.module.css';
 
 function Gallery() {
-  const imageSlides = [
-    {
-      title: "2020 Beige",
-      images: [
-        { src: img1, alt: "이미지1" },
-        { src: img2, alt: "이미지2" },
-        { src: img3, alt: "이미지3" },
-        { src: img4, alt: "이미지4" },
-      ],
-    },
-    {
-      title: "2021 Red",
-      images: [
-        { src: img2, alt: "이미지1" },
-        { src: img3, alt: "이미지2" },
-        { src: img4, alt: "이미지3" },
-        { src: img1, alt: "이미지4" },
-      ],
-    },
-    {
-      title: "2022 Gyeongju",
-      images: [
-        { src: img3, alt: "이미지1" },
-        { src: img4, alt: "이미지2" },
-        { src: img2, alt: "이미지3" },
-        { src: img1, alt: "이미지4" },
-      ],
-    },
-    {
-      title: "2022 Black",
-      images: [
-        { src: img4, alt: "이미지1" },
-        { src: img3, alt: "이미지2" },
-        { src: img2, alt: "이미지3" },
-        { src: img1, alt: "이미지4" },
-      ],
-    },
-    {
-      title: "2023 Blue",
-      images: [
-        { src: img2, alt: "이미지1" },
-        { src: img3, alt: "이미지2" },
-        { src: img4, alt: "이미지3" },
-        { src: img1, alt: "이미지4" },
-      ],
-    },
-    {
-      title: "2024 Europe",
-      images: [
-        { src: img2, alt: "이미지1" },
-        { src: img3, alt: "이미지2" },
-        { src: img4, alt: "이미지3" },
-        { src: img1, alt: "이미지4" },
-      ],
-    },
-  ];
-
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
   const [activeImageIndex, setActiveImageIndex] = useState(0);
   const slideRefs = useRef([]);
@@ -152,5 +100,92 @@ function Gallery() {
     </section>
   );
 }
+
+const imageSlides = [
+  {
+    title: "2020 Beige",
+    images: [
+      { src: img1, alt: "이미지1" },
+      { src: img2, alt: "이미지2" },
+      { src: img3, alt: "이미지3" },
+      { src: img4, alt: "이미지4" },
+    ],
+  },
+  {
+    title: "2021 Red",
+    images: [
+      { src: img2, alt: "이미지1" },
+      { src: img3, alt: "이미지2" },
+      { src: img4, alt: "이미지3" },
+      { src: img1, alt: "이미지4" },
+    ],
+  },
+  {
+    title: "2022 Gyeongju",
+    images: [
+      { src: img3, alt: "이미지1" },
+      { src: img4, alt: "이미지2" },
+      { src: img2, alt: "이미지3" },
+      { src: img1, alt: "이미지4" },
+    ],
+  },
+  {
+    title: "2022 Black",
+    images: [
+      { src: img4, alt: "이미지1" },
+      { src: img3, alt: "이미지2" },
+      { src: img2, alt: "이미지3" },
+      { src: img1, alt: "이미지4" },
+    ],
+  },
+  {
+    title: "2023 Blue",
+    images: [
+      { src: img2, alt: "이미지1" },
+      { src: img3, alt: "이미지2" },
+      { src: img4, alt: "이미지3" },
+      { src: img1, alt: "이미지4" },
+    ],
+  },
+  {
+    title: "2024 Europe",
+    images: [
+      // { src: paris1, alt: "이미지1" },
+      // { src: paris3, alt: "이미지3" },
+      { src: paris2, alt: "이미지2" },
+      { src: paris4, alt: "이미지4" },
+      { src: paris5, alt: "이미지5" },
+      { src: paris6, alt: "이미지6" },
+      { src: paris7, alt: "이미지7" },
+      { src: paris8, alt: "이미지8" },
+      { src: paris9, alt: "이미지9" },
+      { src: paris10, alt: "이미지10" },
+      { src: rome1, alt: "이미지1" },
+      { src: rome2, alt: "이미지2" },
+      // { src: rome3, alt: "이미지3" },
+      // { src: rome4, alt: "이미지4" },
+      { src: rome5, alt: "이미지5" },
+      { src: rome6, alt: "이미지6" },
+      // { src: rome7, alt: "이미지7" },
+      // { src: rome8, alt: "이미지8" },
+      // { src: rome9, alt: "이미지9" },
+      { src: rome10, alt: "이미지10" },
+      // { src: rome11, alt: "이미지11" },
+      { src: rome12, alt: "이미지12" },
+      { src: rome13, alt: "이미지13" },
+      { src: rome14, alt: "이미지14" },
+      { src: rome15, alt: "이미지15" },
+      // { src: rome16, alt: "이미지16" },
+      { src: rome17, alt: "이미지17" },
+      // { src: rome18, alt: "이미지18" },
+      { src: rome19, alt: "이미지19" },
+      // { src: rome20, alt: "이미지20" },
+      // { src: rome21, alt: "이미지21" },
+      { src: rome22, alt: "이미지22" },
+      // { src: rome23, alt: "이미지23" }
+    
+    ],
+  },
+];
 
 export default Gallery;
